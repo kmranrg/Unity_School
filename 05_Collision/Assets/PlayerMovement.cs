357f12282a42e65e7c0d571fd542a3303c6418a0
Kumar Anurag <me@kmranrg.com>
2024-10-02 15:37:26 -0600

    collision

---
+++ 05_Collision/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // this is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // we marked this as FixedUpdate because we are using it to mess with Physics
+    void FixedUpdate()
+    {
+        // adding a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // add a force of 2000 on the z-axis
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
